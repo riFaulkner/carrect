ed9c4d23a97e6b8c79f8d804c8e64d128366e54a
Rick <Richard.faulkner93@gmail.com>
2019-04-22 19:55:19 -0600

    Add:    Unit tests around fermEntityMethods    BreweryId to measureFermEntity    JSR bean validation to measureFermEntity    MeasureFermEntityQuery by both fermId and breweryId    Brewery level security to FermMeasure controller    Brewery level security annotation
    Update:
       fermIdFk -> fermId. and Integer-> int
       JWT implementation adds list of user authorities on to the Authority object when requests come through
       Controller advice NO_CONTENT errors log.warn and return null payload with correct HTTP status

--- src/main/java/com/rifaulkner/Application.java
+++ src/main/java/com/rifaulkner/Application.java
@@ -5,7 +5,14 @@ package com.rifaulkner;
  */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("com.rifaulkner.*")
+//@ComponentScan(basePackages = { "com.rifaulkner.*" })
+@ComponentScan("com.rifaulkner.*")
+@EntityScan("com.rifaulkner.*")
 @SpringBootApplication
 public class Application {
 
